4119e7182f23168052af9f3e074029e0a0a40c15
hmd <hmd19570@gmail.com>
2023-08-07 23:29:15 +0200

    Aflevering 23

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,34 @@
 <script>
+	import Header from "./Components/Header.svelte"
+	import Footer from "./Components/Footer.svelte"
+	import Tabs from "./Shared/Tabs.svelte"
+  	import CreatePollForm from "./Components/CreatePollForm.svelte";
 	
+
+	//Tabs
+	let items=['Current Polls', 'Add new Poll'];
+	let activeItem="Current Polls";
+
+	const tabChange=(e) => {
+		activeItem=e.detail;
+	}
 </script>
 
-
+<Header />
+<main>
+	<Tabs items={items}  activeItem={activeItem} on:tabChange={tabChange}/>
+	
+	{#if activeItem==="Current Polls"}
+		<p>Current polls component shown here</p>
+	{:else if activeItem==="Add new Poll"}
+		<CreatePollForm />
+	{/if}
+</main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+main {
+	max-width: 960px;
+	margin: 20px auto;
+}
 </style>
---
+++ src/Components/Footer.svelte
@@ -0,0 +1,21 @@
+<footer>
+    <div class="copyright">
+        Copyright 2023 Hubert van Dongen
+    </div>
+</footer>
+
+<style>
+
+footer {
+    padding: 40px;
+    text-align: center;
+}
+
+.copyright {
+    color: #aaa;
+    font-size: 14px;
+    padding: 20px;
+    border-top: 1px solid #ddd ;
+    display: inline-block;
+}
+</style>
